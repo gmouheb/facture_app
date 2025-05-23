674c284411090285b844d5d0483fff9f6de14e6f
mouheb <ghabrimouheb@gmail.com>
2025-05-23 18:24:05 +0100

    m

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 apt-get update
 apt-get install -y build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi
 apt-get install -y libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+
+# Create the expected directory structure
+mkdir -p /opt/render/project/src/facture_app
+cp -r . /opt/render/project/src/facture_app/
